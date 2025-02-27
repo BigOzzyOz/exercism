6ad586948b721b4137cdd4d7879a3a00871b3b98
BigOzzyOz <60309436+BigOzzyOz@users.noreply.github.com>
2025-02-27 13:48:40 +0100

    finish bird-watcher

--- done/bird-watcher/bird-watcher.js
+++ done/bird-watcher/bird-watcher.js
@@ -11,7 +11,12 @@
  * @returns {number} total bird count
  */
 export function totalBirdCount(birdsPerDay) {
-  throw new Error('Please implement the totalBirdCount function');
+  let sum = 0;
+  for (let i = 0; i < birdsPerDay.length; i++) {
+    sum += birdsPerDay[i];
+    console.log('function1', sum);
+  }
+  return sum;
 }
 
 /**
@@ -22,7 +27,12 @@ export function totalBirdCount(birdsPerDay) {
  * @returns {number} birds counted in the given week
  */
 export function birdsInWeek(birdsPerDay, week) {
-  throw new Error('Please implement the birdsInWeek function');
+  let sum = 0;
+  for (let i = 7 * (week - 1); i < 7 * week; i++) {
+    sum += birdsPerDay[i];
+    console.log('function2', sum);
+  }
+  return sum;
 }
 
 /**
@@ -33,5 +43,10 @@ export function birdsInWeek(birdsPerDay, week) {
  * @returns {number[]} corrected bird count data
  */
 export function fixBirdCountLog(birdsPerDay) {
-  throw new Error('Please implement the fixBirdCountLog function');
+  for (let i = 0; i < birdsPerDay.length; i++) {
+    if (i === 0 || i % 2 === 0) {
+      birdsPerDay[i] += 1;
+    }
+  }
+  return birdsPerDay;
 }
